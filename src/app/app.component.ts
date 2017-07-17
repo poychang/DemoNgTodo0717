de00093c03f04e9fff46e17e5459634f47dc9681
PoyChang <poypost@gmail.com>
2017-07-17 15:07:02 +0800

    任務 06：套用事件繫結 (Event Binding) 使用小括號 ( ) 繫結事件，使用兩種傳遞 event 事件的寫法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   inputHint = 'What needs to be done?';
+  todos: any[] = [];
+
+  /** 不內含轉換的寫法，直接傳入 event 事件
+   * 樣版用法 (keyup.enter)="addTodo($event)"
+   */
+  addTodo($event: Event) {
+    const target = <HTMLInputElement>$event.target;
+    console.log(target.value);
+    this.todos.push(target.value);
+    target.value = '';
+  }
+
+  /** 內含轉換的寫法，傳入 event
+   * 的當前對象，在參數列中設定型別（EventTarget->HTMLInputElement，內含有轉型的效果）
+   * 樣版用法 (keyup.enter)="addTodo($event.target)"
+   */
+  addTodo2(target: HTMLInputElement) {
+    console.log(target.value);
+    this.todos.push(target.value);
+    target.value = '';
+  }
 }
